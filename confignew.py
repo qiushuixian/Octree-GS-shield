753f213589e354e36d585421c7c562da67534dcf
qiushuixian <148749336+qiushuixian@users.noreply.github.com>
2025-06-30 21:03:33 +0800

    Create confignew.py

---
+++ confignew.py
@@ -0,0 +1,17 @@
+viewmatrix_new= [
+    [0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0]
+]
+flag_mask=0
+loss_of_para=0.0
+flag_para=0
+learnable_para_mid=[]
+before_mask_sum=0
+after_mask_sum=0
+R_mid=0
+T_mid=0
+flag1=0
+fx_mid=0
+fy_mid=0
