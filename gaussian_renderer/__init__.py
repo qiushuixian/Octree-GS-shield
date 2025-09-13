5a3272fe2959631f72b93b2b80de8e6144e650cb
qiushuixian <148749336+qiushuixian@users.noreply.github.com>
2025-09-13 08:49:40 +0800

    Update __init__.py

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -151,6 +151,118 @@ def generate_neural_gaussians(viewpoint_camera, pc : GaussianModel, visible_mask
         return xyz, color, opacity, scaling, rot, neural_opacity, mask
     else:
         return xyz, color, opacity, scaling, rot
+def generate_neural_gaussians_no_mask(viewpoint_camera, pc : GaussianModel, visible_mask=None, is_training=False,  ape_code=-1):
+    ## view frustum filtering for acceleration    
+    if visible_mask is None:
+        visible_mask = torch.ones(pc.get_anchor.shape[0], dtype=torch.bool, device = pc.get_anchor.device)
+
+    anchor = pc.get_anchor[visible_mask]
+    feat = pc.get_anchor_feat[visible_mask]
+    level = pc.get_level[visible_mask]
+    grid_offsets = pc._offset[visible_mask]
+    grid_scaling = pc.get_scaling[visible_mask]
+
+    ## get view properties for anchor
+    ob_view = anchor - viewpoint_camera.camera_center
+    # dist
+    ob_dist = ob_view.norm(dim=1, keepdim=True)
+    # view
+    ob_view = ob_view / ob_dist
+
+    ## view-adaptive feature
+    if pc.use_feat_bank:
+        if pc.add_level:
+            cat_view = torch.cat([ob_view, level], dim=1)
+        else:
+            cat_view = ob_view
+        
+        bank_weight = pc.get_featurebank_mlp(cat_view).unsqueeze(dim=1) # [n, 1, 3]
+
+        ## multi-resolution feat
+        feat = feat.unsqueeze(dim=-1)
+        feat = feat[:,::4, :1].repeat([1,4,1])*bank_weight[:,:,:1] + \
+            feat[:,::2, :1].repeat([1,2,1])*bank_weight[:,:,1:2] + \
+            feat[:,::1, :1]*bank_weight[:,:,2:]
+        feat = feat.squeeze(dim=-1) # [n, c]
+
+    if pc.add_level:
+        cat_local_view = torch.cat([feat, ob_view, ob_dist, level], dim=1) # [N, c+3+1+1]
+        cat_local_view_wodist = torch.cat([feat, ob_view, level], dim=1) # [N, c+3+1]
+    else:
+        cat_local_view = torch.cat([feat, ob_view, ob_dist], dim=1) # [N, c+3+1]
+        cat_local_view_wodist = torch.cat([feat, ob_view], dim=1) # [N, c+3]
+
+    if pc.appearance_dim > 0:
+        if is_training or ape_code < 0:
+            camera_indicies = torch.ones_like(cat_local_view[:,0], dtype=torch.long, device=ob_dist.device) * viewpoint_camera.uid
+            appearance = pc.get_appearance(camera_indicies)
+        else:
+            camera_indicies = torch.ones_like(cat_local_view[:,0], dtype=torch.long, device=ob_dist.device) * ape_code[0]
+            appearance = pc.get_appearance(camera_indicies)
+            
+    # get offset's opacity
+    if pc.add_opacity_dist:
+        neural_opacity = pc.get_opacity_mlp(cat_local_view) # [N, k]
+    else:
+        neural_opacity = pc.get_opacity_mlp(cat_local_view_wodist)
+    
+    if pc.dist2level=="progressive":
+        prog = pc._prog_ratio[visible_mask]
+        transition_mask = pc.transition_mask[visible_mask]
+        prog[~transition_mask] = 1.0
+        neural_opacity = neural_opacity * prog
+
+    # opacity mask generation
+    neural_opacity = neural_opacity.reshape([-1, 1])
+    mask = (neural_opacity>0.0)
+    mask = mask.view(-1)
+
+    # select opacity 
+    opacity = torch.clamp(neural_opacity,min=0.0)
+
+    # get offset's color
+    if pc.appearance_dim > 0:
+        if pc.add_color_dist:
+            color = pc.get_color_mlp(torch.cat([cat_local_view, appearance], dim=1))
+        else:
+            color = pc.get_color_mlp(torch.cat([cat_local_view_wodist, appearance], dim=1))
+    else:
+        if pc.add_color_dist:
+            color = pc.get_color_mlp(cat_local_view)
+        else:
+            color = pc.get_color_mlp(cat_local_view_wodist)
+    color = color.reshape([anchor.shape[0]*pc.n_offsets, 3])# [mask]
+
+    # get offset's cov
+    if pc.add_cov_dist:
+        scale_rot = pc.get_cov_mlp(cat_local_view)
+    else:
+        scale_rot = pc.get_cov_mlp(cat_local_view_wodist)
+    scale_rot = scale_rot.reshape([anchor.shape[0]*pc.n_offsets, 7]) # [mask]
+    
+    # offsets
+    offsets = grid_offsets.view([-1, 3]) # [mask]
+    
+    # combine for parallel masking
+    concatenated = torch.cat([grid_scaling, anchor], dim=-1)
+    concatenated_repeated = repeat(concatenated, 'n (c) -> (n k) (c)', k=pc.n_offsets)
+    concatenated_all = torch.cat([concatenated_repeated, color, scale_rot, offsets], dim=-1)
+    masked = concatenated_all
+    scaling_repeat, repeat_anchor, color, scale_rot, offsets = masked.split([6, 3, 3, 7, 3], dim=-1)
+    
+    # post-process cov
+    scaling = scaling_repeat[:,3:] * torch.sigmoid(scale_rot[:,:3]) # * (1+torch.sigmoid(repeat_dist))
+    rot = pc.rotation_activation(scale_rot[:,3:7])
+    
+    # post-process offsets to get centers for gaussians
+    offsets = offsets * scaling_repeat[:,:3]
+    
+    xyz = repeat_anchor + offsets 
+
+    if is_training:
+        return xyz, color, opacity, scaling, rot, neural_opacity, mask
+    else:
+        return xyz, color, opacity, scaling, rot
 
 def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, scaling_modifier=1.0, visible_mask=None, retain_grad=False, ape_code=-1):
     """
@@ -193,6 +305,19 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
         debug=pipe.debug
     )
 
+    if (is_training==0):
+        scaling_prod = scaling.prod(dim=1)*10000  # [N]
+        z_vals =xyz[:, 2].abs()                   # [N]
+        score = ((scaling_prod * (opacity.flatten().abs())**12.95 )/z_vals.abs()).abs()
+        mask_filter = score >= 1.0e-17
+        '''
+        xyz =xyz[mask_filter]
+        screenspace_points = screenspace_points[mask_filter]
+        color = color[mask_filter]
+        scaling = scaling[mask_filter]
+        opacity = opacity[mask_filter]
+        rot=rot[mask_filter]
+        '''
     rasterizer = GaussianRasterizer(raster_settings=raster_settings)
     
     # Rasterize visible Gaussians to image, obtain their radii (on screen). 
